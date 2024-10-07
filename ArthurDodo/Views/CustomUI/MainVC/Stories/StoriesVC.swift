7aca49b3689b9b6de6bca65d0f414c876885eb4f
Kirill Sklyarov <k.sklyarov@yandex.ru>
2024-10-07 20:24:05 +0300

    - пробую восстановить

---
+++ ArthurDodo/Views/CustomUI/MainVC/Stories/StoriesVC.swift
@@ -0,0 +1,40 @@
+//
+//  StoriesVC.swift
+//  ArthurDodo
+//
+//  Created by Kirill Sklyarov on 07.10.2024.
+//
+
+import UIKit
+
+final class StoriesVC: UIViewController {
+
+//    private lazy var backgroundView = BackgroundStoriesView()
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        setupUI()
+//        dataBinding()
+//    }
+
+//    private func dataBinding() {
+//        backgroundView.onDismissButtonTapped = { [weak self] in
+//            self?.dismiss(animated: true)
+//        }
+//    }
+//}
+//
+//private extension StoriesVC {
+//    func setupUI() {
+//        view.backgroundColor = AppColors.backgroundBlack
+//
+//        view.addSubviews(backgroundView)
+//
+//        NSLayoutConstraint.activate([
+//            backgroundView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+//            backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//            backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+//            backgroundView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+//        ])
+//    }
+}
